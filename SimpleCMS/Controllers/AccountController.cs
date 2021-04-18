3e83995339ce9b6def4a7ea2a83c5c58fcc33c93
CZOndrej <on.krejci@gmail.com>
2021-04-18 14:23:23 +0200

    Login redirections

---
+++ SimpleCMS/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCMS.Models;
+using SimpleCMS.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCMS.Controllers
+{
+    [Authorize(Roles = "SysAdmin")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(AdminController.Index), "Admin");
+            }
+            ModelState.AddModelError(string.Empty, "Neplatné přihlašovací údaje");
+            return View(model);
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
--- SimpleCMS/Controllers/AdminController.cs
+++ SimpleCMS/Controllers/AdminController.cs
@@ -1,47 +1,18 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using SimpleCMS.Models;
-using SimpleCMS.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SimpleCMS.Controllers
-{
-    [Authorize]
+{   
+    [Authorize (Roles = "SysAdmin")]
     public class AdminController : Controller
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
-        public AdminController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _roleManager = roleManager;
-        }
-
-        public IActionResult Index() => View();
-
-        [AllowAnonymous]
-        public IActionResult Login()
+        public IActionResult Index()
         {
             return View();
-        }
-        [AllowAnonymous]
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
-
-            if (result.Succeeded)
-            {
-                return View("Index");
-            }
-            ModelState.AddModelError(string.Empty, "Neplatné přihlašovací údaje");
-            return View(model);
         }
     }
 }
