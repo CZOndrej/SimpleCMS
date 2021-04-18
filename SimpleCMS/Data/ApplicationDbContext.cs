e7ab2566413336f68a3292d77c482886f8b635d4
CZOndrej <on.krejci@gmail.com>
2021-04-18 11:39:46 +0200

    Add project files.

---
+++ SimpleCMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SimpleCMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
